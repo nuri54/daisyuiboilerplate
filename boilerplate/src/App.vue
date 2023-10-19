126313a8a792fe871e06ecf40401d376c218c5c7
nuri <hendennurullah@gmail.com>
2023-10-19 17:28:03 +0200

    initial setup

---
+++ boilerplate/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <button class="btn">Button</button>
+  <button class="btn btn-neutral">Neutral</button>
+  <button class="btn btn-primary">Primary</button>
+  <button class="btn btn-secondary">Secondary</button>
+  <button class="btn btn-accent">Accent</button>
+  <button class="btn btn-ghost">Ghost</button>
+  <button class="btn btn-link">Link</button>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
